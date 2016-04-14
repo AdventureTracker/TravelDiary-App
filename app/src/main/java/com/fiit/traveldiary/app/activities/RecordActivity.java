0877a6d3bd7046dd5d225d581730cabc206c744a
Barbora Celesova <barbora.celesova@gmail.com>
2016-04-14 11:06:51 +0200

    RecordActivity

---
+++ app/src/main/java/com/fiit/traveldiary/app/activities/RecordActivity.java
@@ -0,0 +1,26 @@
+package com.fiit.traveldiary.app.activities;
+
+/**
+ * Created by Barbora on 14.4.2016.
+ */
+import android.os.Bundle;
+import android.app.Activity;
+import android.widget.TextView;
+import android.widget.GridView;
+
+public class RecordActivity extends Activity  {
+
+    private TextView description;
+    private GridView galleryGridView;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.record);
+
+        description = (TextView) findViewById(R.id.description);
+        galleryGridView = (GridView) findViewById(R.id.galleryGridView);
+    }
+
+}
+
